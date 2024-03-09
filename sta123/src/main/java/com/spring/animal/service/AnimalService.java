695e3623f03165d21689f2e8f0b7cba5aacaf237
networkSorcerer <gigattfyhig@gmail.com>
2024-03-10 00:17:13 +0900

    sta

--- sta123/src/main/java/com/spring/animal/service/AnimalService.java
+++ sta123/src/main/java/com/spring/animal/service/AnimalService.java
@@ -7,6 +7,7 @@ import com.spring.animal.vo.AnimalVO;
 
 public interface AnimalService {
 	public List<AnimalVO> animalList(AnimalVO avo);
+	
 	public int animalListCnt(AnimalVO avo);
 	public AnimalVO animalDetail(AnimalVO avo); 
 	public int pwdConfirm(AnimalVO avo);
